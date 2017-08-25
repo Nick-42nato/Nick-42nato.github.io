7bb9e462018fc3328e4990b58c45871e7f7618a8
Nick-42nato <nfortunato007@gmail.com>
2017-08-25 15:24:32 -0700

    Delete engine.js

--- engine.js
+++
@@ -1,2 +0,0 @@
-TweenMax.staggerFrom(document.querySelectorAll("[data-bit]"), 0.75, { y: 200, autoAlpha: 0 }, -0.15);
-TweenMax.from(document.querySelectorAll("[data-allBits]"), 1.5, { y: 100, ease: Back.easeOut.config(4) });
